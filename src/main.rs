9306bf7add276a707a1f0317857d9a8e6dd9480c
Juliandev02 <juli.mackenstein@gmail.com>
2024-01-21 12:01:28 +0000

    added command handling

--- src/main.rs
+++ src/main.rs
@@ -11,104 +11,41 @@
 //! There are two components to the crate, offering implementations of the
 //! server network daemon and client local forwarding proxy. Both are public
 //! members and can be run programmatically with a Tokio 1.0 runtime.
+use anyhow::Result;
 
-use crate::cli::ARGS;
+use tunneled::client::Client;
+use tunneled::server::Server;
+
+use crate::cli::{ARGS, OPTIONS};
 use crate::cli::args::Command;
+use stblib::colors::{BOLD, C_RESET, RED, RESET};
 
 mod cli;
-mod colors;
 mod commands;
 mod statics;
-
-
-/*
-#[derive(Parser, Debug)]
-#[clap(author, version, about)]
-struct Args {
-    #[clap(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand, Debug)]
- enum Command {
-    /// Starts a local proxy to the remote server.
-    Local {
-        /// The local port to expose.
-        local_port: u16,
-
-        /// The local host to expose.
-        #[clap(short, long, value_name = "HOST", default_value = "localhost")]
-        local_host: String,
-
-        /// Address of the remote server to expose local ports to.
-        #[clap(short, long, env = "BORE_SERVER")]
-        to: String,
-
-        /// Optional port on the remote server to select.
-        #[clap(short, long, default_value_t = 0)]
-        port: u16,
-
-        /// Optional secret for authentication.
-        #[clap(short, long, env = "BORE_SECRET", hide_env_values = true)]
-        secret: Option<String>,
-    },
-
-    /// Runs the remote proxy server.
-    Server {
-        /// Minimum accepted TCP port number.
-        #[clap(long, default_value_t = 1024)]
-        min_port: u16,
-
-        /// Maximum accepted TCP port number.
-        #[clap(long, default_value_t = 65535)]
-        max_port: u16,
-
-        /// Optional secret for authentication.
-        #[clap(short, long, env = "BORE_SECRET", hide_env_values = true)]
-        secret: Option<String>,
-    },
-} */
-
-/* #[tokio::main]
-async fn run(command: Command) -> Result<()> {
-    match command {
-        Command::Local {
-            local_host,
-            local_port,
-            to,
-            port,
-            secret,
-        } => {
-            let client = Client::new(&local_host, local_port, &to, port, secret.as_deref()).await?;
-            client.listen().await?;
-        }
-        Command::Server {
-            min_port,
-            max_port,
-            secret,
-        } => {
-            let port_range = min_port..=max_port;
-            if port_range.is_empty() {
-                Args::command()
-                    .error(ErrorKind::InvalidValue, "port range is empty")
-                    .exit();
-            }
-            Server::new(port_range, secret.as_deref()).listen().await?;
-        }
-    }
-
-    Ok(())
-} */
+mod auth;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result <()> {
     match ARGS.command {
         Command::Local => {
+            let client = Client::new(
+                &OPTIONS.client_options.host, OPTIONS.client_options.port,
+                &OPTIONS.client_options.server, 0,
+                OPTIONS.client_options.secret.as_deref()
+            ).await.unwrap();
 
+            client.listen().await.unwrap();
         },
         Command::Server => {
-
+            let port_range = OPTIONS.server_options.min_port..=OPTIONS.server_options.max_port;
+            if port_range.is_empty() {
+                eprintln!("{RED}{BOLD} ! {RESET} Port range is empty{C_RESET}");
+            }
+            Server::new(port_range, OPTIONS.server_options.secret.as_deref()).listen().await?;
         }
         Command::None => commands::help::help(),
     }
+
+    Ok(())
 }
