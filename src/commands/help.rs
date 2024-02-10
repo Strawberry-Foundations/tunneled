98c8c390a51bdf997202c84909faba5d07d03b66
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 00:02:40 +0000

    Fixed help message

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -12,9 +12,10 @@ pub fn help() {
     {CYAN}{BOLD}local <port>:{C_RESET} Starts a local proxy to the remote server
      {BOLD}↳ {MAGENTA}Options:{C_RESET}
             {CYAN}{BOLD}-u, --use <server>{C_RESET}      Select your target server for tunneling your traffic
-            {CYAN}{BOLD}-l, --local-host <host>{C_RESET} The address to expose      {GREEN}{BOLD}[default: localhost]{C_RESET}
-            {CYAN}{BOLD}-p, --port <port>{C_RESET}       The port to expose         {GREEN}{BOLD}[optional]{C_RESET}
-            {CYAN}{BOLD}-s, --secret <port>{C_RESET}     Secret for authentication  {GREEN}{BOLD}[optional]{C_RESET}
+            {CYAN}{BOLD}-l, --local-host <host>{C_RESET} The address to expose                 {GREEN}{BOLD}[default: localhost]{C_RESET}
+            {CYAN}{BOLD}-p, --port <port>{C_RESET}       The port to expose                    {GREEN}{BOLD}[optional]{C_RESET}
+            {CYAN}{BOLD}-s, --secret <secret>{C_RESET}   Secret for authentication             {GREEN}{BOLD}[optional]{C_RESET}
+            {CYAN}{BOLD}-a, --auth{C_RESET}              Use Strawberry ID for Authentication  {GREEN}{BOLD}[optional]{C_RESET}
 
     {CYAN}{BOLD}auth:{C_RESET} Authenticate with your Strawberry ID
      {BOLD}↳ {MAGENTA}Options:{C_RESET}
