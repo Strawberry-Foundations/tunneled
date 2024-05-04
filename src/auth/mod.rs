1267cde3365c423c9f97e6aaf6379bb851faf680
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-04 21:40:17 +0200

    Strawberry ID changes & optimizations

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,2 +1,18 @@
+use crate::auth::strawberry_id::StrawberryId;
+
 pub mod strawberry_id;
-pub mod id;
+pub mod authenticator;
+
+pub struct Auth;
+
+impl Auth {
+    pub fn strawberry_id() -> StrawberryId {
+        StrawberryId {
+            email: String::new(),
+            full_name: String::new(),
+            profile_picture: String::new(),
+            username: String::new(),
+            token: String::new(),
+        }
+    }
+}
