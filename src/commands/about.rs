d0cc546dfad4e0e59d113cdf783bfb28496eb146
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-15 22:15:01 +0200

    Updated about command

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -1,10 +1,36 @@
-use stblib::colors::{C_RESET, BOLD, UNDERLINE, CYAN};
+use stblib::colors::{C_RESET, BOLD, UNDERLINE, GREEN, LIGHT_GREEN, RED, YELLOW, WHITE, BLUE};
 use crate::statics::VERSION;
 
 pub fn about() {
     println!("\
-{BOLD}{CYAN}{UNDERLINE}Strawberry Tunneled v{}{C_RESET}\n\
-tunneled is a simple CLI tool for making local tcp tunnels
+* ---------- {BLUE}{BOLD}tunneled{C_RESET}{BOLD}{WHITE} ---------- *
+|             v{}             |
+| tunneled is a simple CLI tool  |
+|  for making local tcp tunnels  |
+* ------------------------------ *
+
+* ------------------------------ *
+|    Copyright (C) 2022 - 2024   |
+|     Strawberry Foundations     |
+|                                |
+| Made possible with the help of |
+|     github.com/ekzhang/bore    |
+* ------------------------------ *
+
+* ----------- {BOLD}{YELLOW}WARNING{C_RESET}{BOLD} ---------- *
+|    This program comes with     |
+|     absolutely {RED}{UNDERLINE}NO{C_RESET}{WHITE}{BOLD} warranty     |
+|                                |
+| {LIGHT_GREEN}This is free software, and you{C_RESET}{WHITE}{BOLD} |
+| {LIGHT_GREEN}are welcome to redistribute it{C_RESET}{WHITE}{BOLD} |
+* ------------------------------ *
+
+* ------------- {BOLD}{GREEN}License{C_RESET}{WHITE}{BOLD} ------------- *
+|        GPL-V3 (original MIT)        |
+|         Open Source License         |
+|                                     |
+| https://opensource.org/license/gpl  |
+* ----------------------------------- *{C_RESET}
 ", *VERSION);
     std::process::exit(0);
 }
