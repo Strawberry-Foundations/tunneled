e0366618336ce33f7819c906fdc94a38527d8608
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-15 14:56:39 +0200

    feat: added compose command

---
+++ src/commands/compose.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::Read;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Service {
+    pub name: String,
+    pub port: u16,
+    #[serde(rename = "static-port")]
+    pub static_port: Option<u16>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Services {
+    pub services: Vec<Service>,
+}
+
+pub fn read_service_file(file_path: &str) -> Result<Services, Box<dyn std::error::Error>> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let services: Services = serde_yaml::from_str(&contents)?;
+    Ok(services)
+}
+
+
+pub fn compose() -> anyhow::Result<()> {
+    let services = read_service_file("services.yml").unwrap();
+
+    Ok(())
+}
