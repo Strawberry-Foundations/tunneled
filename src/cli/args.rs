69d9460e380a1ab31db389d1994cf40324c3de32
Juliandev02 <julian@strawberryfoundations.org>
2024-10-11 18:01:44 +0200

    feat: add plugins

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -7,6 +7,7 @@ pub enum Command {
     Compose,
     Auth,
     About,
+    Plugin,
     None
 }
 
@@ -72,6 +73,7 @@ impl Args {
             "auth" => args.command = Command::Auth,
             "about" => args.command = Command::About,
             "compose" => args.command = Command::Compose,
+            "plugin" => args.command = Command::Plugin,
             _ => args.command = Command::None,
         }
 
