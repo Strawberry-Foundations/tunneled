0ab3fcc2a7d3d6bdfb8e1643898af269f4da5925
Juliandev02 <julian@strawberryfoundations.org>
2024-11-11 18:52:03 +0100

    feat: optimize arg parser

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,6 +1,8 @@
 use std::env;
 use stblib::colors::{BOLD, C_RESET, RED, RESET};
+use std::collections::HashMap;
 
+#[derive(Clone)]
 pub enum Command {
     Local,
     Server,
@@ -8,7 +10,7 @@ pub enum Command {
     Auth,
     About,
     Plugin,
-    None
+    None,
 }
 
 #[derive(Default)]
@@ -18,7 +20,7 @@ pub struct ServerOptions {
     pub secret: Option<String>,
     pub require_id: bool,
     pub control_port: u16,
-    pub config_file: Option<String>
+    pub config_file: Option<String>,
 }
 
 #[derive(Default)]
@@ -30,9 +32,8 @@ pub struct ClientOptions {
     pub auth: bool,
     pub control_port: u16,
     pub static_port: Option<u16>,
-    pub compose_file: Option<String>
+    pub compose_file: Option<String>,
 }
-
 
 #[derive(Default)]
 pub struct Options {
@@ -49,209 +50,149 @@ pub struct Args {
 
 impl Args {
     pub fn collect() -> Self {
-        let mut args = Self {
-            args: vec![],
+        let args: Vec<String> = env::args().skip(1).collect();
+
+        let mut result = Args {
+            args: args.clone(),
             command: Command::None,
-            command_str: String::new(),
-            options: Options { ..Default::default() }
+            command_str: args.get(0).cloned().unwrap_or_default(),
+            options: Options::default(),
         };
 
-        let collector: Vec<String> = env::args().collect();
+        // Using a HashMap for command lookup for scalability
+        let command_map = HashMap::from([
+            ("local", Command::Local),
+            ("server", Command::Server),
+            ("auth", Command::Auth),
+            ("about", Command::About),
+            ("compose", Command::Compose),
+            ("plugin", Command::Plugin),
+        ]);
 
-        if collector.len() <= 1 {
-            return args
-        }
+        result.command = command_map
+            .get(result.command_str.as_str())
+            .cloned()
+            .unwrap_or(Command::None);
 
-        let parser: Vec<String> = env::args().skip(1).collect();
-
-        args.args.clone_from(&parser);
-        args.command_str = parser.clone().first().unwrap().to_string();
-
-        match args.command_str.as_str() {
-            "local" => args.command = Command::Local,
-            "server" => args.command = Command::Server,
-            "auth" => args.command = Command::Auth,
-            "about" => args.command = Command::About,
-            "compose" => args.command = Command::Compose,
-            "plugin" => args.command = Command::Plugin,
-            _ => args.command = Command::None,
-        }
-
-        args
+        result
     }
 
     pub fn collect_options(&mut self) -> Options {
         let mut options = Options {
             server_options: ServerOptions {
                 min_port: 1024,
-                max_port: 65535, 
-                secret: None,
-                require_id: false,
+                max_port: 65535,
                 control_port: 7835,
-                config_file: None,
+                ..Default::default()
             },
             client_options: ClientOptions {
-                host: String::from("localhost"),
+                host: "localhost".to_string(),
                 port: 8080,
-                server: String::from("strawberryfoundations.org"),
-                secret: None,
-                auth: false,
+                server: "strawberryfoundations.org".to_string(),
                 control_port: 7835,
-                static_port: None,
-                compose_file: None,
-            }
+                ..Default::default()
+            },
         };
 
-
-        let mut iter = self.args.clone().into_iter().skip(1);
+        let mut iter = self.args.iter().skip(1);
 
         while let Some(arg) = iter.next() {
             match arg.as_str() {
-                "-p" | "--port" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(port) = val.parse::<u16>() {
-                            options.client_options.port = port;
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid port{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing port{C_RESET}");
-                    }
-                },
-
-                "-u" | "--use" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(server) = val.parse::<String>() {
-                            options.client_options.server = server;
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid server{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing server{C_RESET}");
-                    }
-                },
-
-                "-cp" | "--control-port" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(port) = val.parse::<u16>() {
-                            match &self.command {
-                                Command::Local => options.client_options.control_port = port,
-                                Command::Server => options.server_options.control_port = port,
-                                _ => { }
-                            }
-
-
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid control port{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing control port{C_RESET}");
-                    }
-                },
-
+                "-p" | "--port" => parse_u16(iter.next(), &mut options.client_options.port, "port"),
+                "-u" | "--use" => parse_string(iter.next(), &mut options.client_options.server, "server"),
+                "-cp" | "--control-port" => parse_u16(
+                    iter.next(),
+                    match &self.command {
+                        Command::Local => &mut options.client_options.control_port,
+                        Command::Server => &mut options.server_options.control_port,
+                        _ => continue,
+                    },
+                    "control port",
+                ),
                 "-sp" | "--static-port" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(port) = val.parse::<u16>() {
-                            options.client_options.static_port = Some(port)
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid static port{C_RESET}");
-                            std::process::exit(1);
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing static port{C_RESET}");
-                        std::process::exit(1);
+                    if let Some(port) = parse_optional_u16(iter.next(), "static port") {
+                        options.client_options.static_port = Some(port);
                     }
-                },
-
-                "-h" | "--address" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(host) = val.parse::<String>() {
-                            options.client_options.host = host;
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid address{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing address{C_RESET}");
-                    }
-                },
-
-                "-f" | "--file" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(file) = val.parse::<String>() {
-                            match &self.command {
-                                Command::Compose => options.client_options.compose_file = Option::from(file),
-                                Command::Server => options.server_options.config_file = Option::from(file),
-                                _ => { }
-                            }
-                        } else {
-                            match &self.command {
-                                Command::Compose => eprintln!("{RED}{BOLD} ! {RESET} Invalid compose file{C_RESET}"),
-                                Command::Server => eprintln!("{RED}{BOLD} ! {RESET} Invalid config file{C_RESET}"),
-                                _ => { }
-                            }
-                        }
-                    } else {
-                        match &self.command {
-                            Command::Compose => eprintln!("{RED}{BOLD} ! {RESET} Missing compose file{C_RESET}"),
-                            Command::Server => eprintln!("{RED}{BOLD} ! {RESET} Missing config file{C_RESET}"),
-                            _ => { }
-                        }
-                    }
-                },
-
-                "-s" | "--secret" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(secret) = val.parse::<String>() {
-                            match &self.command {
-                                Command::Local => options.client_options.secret = Option::from(secret),
-                                Command::Server => options.server_options.secret = Option::from(secret),
-                                _ => { }
-                            }
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid secret{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing secret{C_RESET}");
-                    }
-                },
-                "--min-port" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(min_port) = val.parse::<u16>() {
-                            options.server_options.min_port = min_port;
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid minimum port{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing minimum port{C_RESET}");
-                    }
-                },
-
-                "--max-port" => {
-                    if let Some(val) = iter.next() {
-                        if let Ok(max_port) = val.parse::<u16>() {
-                            options.server_options.max_port = max_port;
-                        } else {
-                            eprintln!("{RED}{BOLD} ! {RESET} Invalid maximum port{C_RESET}");
-                        }
-                    } else {
-                        eprintln!("{RED}{BOLD} ! {RESET} Missing maximum port{C_RESET}");
-                    }
-                },
-
+                }
+                "-h" | "--address" => parse_string(iter.next(), &mut options.client_options.host, "address"),
+                "-f" | "--file" => parse_file(
+                    iter.next(),
+                    match &self.command {
+                        Command::Compose => &mut options.client_options.compose_file,
+                        Command::Server => &mut options.server_options.config_file,
+                        _ => continue,
+                    },
+                    "file",
+                ),
+                "-s" | "--secret" => parse_optional_string(
+                    iter.next(),
+                    match &self.command {
+                        Command::Local => &mut options.client_options.secret,
+                        Command::Server => &mut options.server_options.secret,
+                        _ => continue,
+                    },
+                    "secret",
+                ),
+                "--min-port" => parse_u16(iter.next(), &mut options.server_options.min_port, "minimum port"),
+                "--max-port" => parse_u16(iter.next(), &mut options.server_options.max_port, "maximum port"),
                 "-a" | "--auth" => options.client_options.auth = true,
                 "-id" | "--require-id" => options.server_options.require_id = true,
-
-                _ => {
-                    let port = arg.parse::<u16>().unwrap_or_else(|_| {
-                        eprintln!("{RED}{BOLD} ! {RESET} Invalid port{C_RESET}");
+                other => {
+                    if let Ok(port) = other.parse::<u16>() {
+                        options.client_options.port = port;
+                    } else {
+                        eprintln!("{RED}{BOLD} ! {RESET} Invalid port argument: {other}{C_RESET}");
                         std::process::exit(1);
-                    });
-
-                    options.client_options.port = port
+                    }
                 }
             }
         }
 
         options
+    }
+}
+
+fn parse_u16(input: Option<&String>, field: &mut u16, field_name: &str) {
+    if let Some(val) = input {
+        *field = val.parse().unwrap_or_else(|_| {
+            eprintln!("{RED}{BOLD} ! {RESET} Invalid {field_name}{C_RESET}");
+            std::process::exit(1);
+        });
+    } else {
+        eprintln!("{RED}{BOLD} ! {RESET} Missing {field_name}{C_RESET}");
+    }
+}
+
+fn parse_string(input: Option<&String>, field: &mut String, field_name: &str) {
+    if let Some(val) = input {
+        *field = val.clone();
+    } else {
+        eprintln!("{RED}{BOLD} ! {RESET} Missing {field_name}{C_RESET}");
+    }
+}
+
+fn parse_optional_string(input: Option<&String>, field: &mut Option<String>, field_name: &str) {
+    if let Some(val) = input {
+        *field = Some(val.clone());
+    } else {
+        eprintln!("{RED}{BOLD} ! {RESET} Missing {field_name}{C_RESET}");
+    }
+}
+
+
+fn parse_optional_u16(input: Option<&String>, field_name: &str) -> Option<u16> {
+    input.and_then(|val| {
+        val.parse().ok().or_else(|| {
+            eprintln!("{RED}{BOLD} ! {RESET} Invalid {field_name}{C_RESET}");
+            None
+        })
+    })
+}
+
+fn parse_file(input: Option<&String>, field: &mut Option<String>, field_name: &str) {
+    if let Some(val) = input {
+        *field = Some(val.clone());
+    } else {
+        eprintln!("{RED}{BOLD} ! {RESET} Missing {field_name}{C_RESET}");
     }
 }
