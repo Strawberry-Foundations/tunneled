b0b032182b55b0d33e07fbd306c94c3b4870802e
Juliandev02 <julian@strawberryfoundations.org>
2025-07-31 19:56:38 +0200

    fix: fix breaking api changes (libstrawberry)

--- src/core/constants.rs
+++ src/core/constants.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 
-use stblib::logging::Logger;
-use stblib::colors::{BLUE, BOLD, C_RESET, CYAN, GREEN, RED, YELLOW};
+use libstrawberry::colors::{BLUE, BOLD, CYAN, C_RESET, GREEN, RED, YELLOW, GRAY};
+use libstrawberry::logging::Logger;
 
 pub const STRAWBERRY_ID_API: &str = "https://id.strawberryfoundations.org/v2/";
 // pub const STRAWBERRY_ID_API: &str = "http://192.168.0.194:8082/v1/";
@@ -10,32 +10,32 @@ lazy_static! {
     pub static ref VERSION: String = env!("CARGO_PKG_VERSION").to_string();
 
     pub static ref LOGGER: Logger = Logger::new(
-        stblib::logging::featureset::FeatureSet::new(),
-        stblib::logging::formats::LogFormat {
-            info: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {GREEN}[%<levelname>%]{C_RESET}    [%<message>%]"),
-            error: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET}   [%<message>%]"),
-            default: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {BLUE}INIT{C_RESET}    [%<message>%]"),
-            warning: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"),
-            critical: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
-            panic: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
-            extensions: stblib::logging::formats::LogFormatExt {
-                time_fmt: "%Y-%m-%d %H:%M".to_string(),
+        libstrawberry::logging::features::LoggingFeatures::new(),
+        libstrawberry::logging::formats::LogFormat {
+            info: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {GREEN}[%<levelname>%]{C_RESET}    [%<message>%]"),
+            error: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET}   [%<message>%]"),
+            ok: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {BLUE}INIT{C_RESET}    [%<message>%]"),
+            warning: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"),
+            critical: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
+            panic: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
+            log_options: libstrawberry::logging::formats::LogFormatOptions {
+                timestamp_format: "%Y-%m-%d %H:%M:%S".to_string(),
                 levelname_lowercase: false
             },
         }
     );
 
     pub static ref LOGGER_2: Logger = Logger::new(
-        stblib::logging::featureset::FeatureSet::new(),
-        stblib::logging::formats::LogFormat {
-            info: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {CYAN}AUTH{C_RESET}    [%<message>%]"),
-            error: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET}   [%<message>%]"),
-            default: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {BLUE}INIT{C_RESET}    [%<message>%]"),
-            warning: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"),
-            critical: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
-            panic: format!("{C_RESET}{BOLD}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
-            extensions: stblib::logging::formats::LogFormatExt {
-                time_fmt: "%Y-%m-%d %H:%M".to_string(),
+        libstrawberry::logging::features::LoggingFeatures::new(),
+        libstrawberry::logging::formats::LogFormat {
+            info: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {CYAN}AUTH{C_RESET}    [%<message>%]"),
+            error: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET}   [%<message>%]"),
+            ok: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {BLUE}INIT{C_RESET}    [%<message>%]"),
+            warning: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"),
+            critical: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
+            panic: format!("{C_RESET}{BOLD}{GRAY}[%<time>%]{C_RESET} {RED}[%<levelname>%]{C_RESET} [%<message>%]"),
+            log_options: libstrawberry::logging::formats::LogFormatOptions {
+                timestamp_format: "%Y-%m-%d %H:%M:%S".to_string(),
                 levelname_lowercase: false
             },
         }
--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,22 @@
 //! server network daemon and client local forwarding proxy. Both are public
 //! members and can be run programmatically with a Tokio 1.0 runtime.
 use anyhow::Result;
-use stblib::colors::{BOLD, C_RESET, RED, RESET};
+use libstrawberry::colors::{BOLD, C_RESET, RED, RESET};
 
+use crate::cli::args::Command;
+use crate::cli::{ARGS, OPTIONS};
+use crate::commands::auth::auth;
+use crate::commands::compose::compose;
 use crate::commands::local::Client;
 use crate::commands::server::{read_config_file, Server};
-use crate::commands::compose::compose;
-use crate::commands::auth::auth;
-
-use core::auth::Auth;
-use crate::cli::{ARGS, OPTIONS};
-use crate::cli::args::Command;
+use crate::core::auth::Auth;
 
 pub mod cli;
 pub mod commands;
-mod core;
+pub mod core;
 
 #[tokio::main]
-async fn main() -> Result <()> {
+async fn main() -> Result<()> {
     match ARGS.command {
         Command::Local => {
             let client = Client::new(
@@ -39,8 +38,10 @@ async fn main() -> Result <()> {
                 OPTIONS.client_options.static_port,
                 OPTIONS.client_options.control_port,
                 OPTIONS.client_options.auth,
-                None
-            ).await.unwrap_or_else(|err| {
+                None,
+            )
+            .await
+            .unwrap_or_else(|err| {
                 eprintln!("{RED}{BOLD} ! {C_RESET} {err}");
                 std::process::exit(1)
             });
@@ -50,12 +51,12 @@ async fn main() -> Result <()> {
                 std::process::exit(1)
             });
         }
-        Command::Compose => {
-            compose(OPTIONS.client_options.compose_file.as_deref()).await.unwrap_or_else(|err| {
+        Command::Compose => compose(OPTIONS.client_options.compose_file.as_deref())
+            .await
+            .unwrap_or_else(|err| {
                 eprintln!("{RED}{BOLD} ! {C_RESET} {err}");
                 std::process::exit(1)
-            })
-        }
+            }),
         Command::Server => {
             if let Some(config_file) = OPTIONS.server_options.config_file.as_deref() {
                 let config = read_config_file(config_file).unwrap_or_else(|err| {
@@ -73,25 +74,33 @@ async fn main() -> Result <()> {
                     config.server.host.control_port.unwrap_or(7835),
                     config.server.auth.require_id.unwrap_or(false),
                     config.server.auth.allow_static_port.unwrap_or_default(),
-                    config.server.host.tunnels_addr.as_deref().unwrap_or("0.0.0.0").to_string(),
-                ).listen().await?;
-            }
-            else {
+                    config
+                        .server
+                        .host
+                        .tunnels_addr
+                        .as_deref()
+                        .unwrap_or("0.0.0.0")
+                        .to_string(),
+                )
+                .listen()
+                .await?;
+            } else {
                 let port_range = OPTIONS.server_options.min_port..=OPTIONS.server_options.max_port;
                 if port_range.is_empty() {
                     eprintln!("{RED}{BOLD} ! {RESET} Port range is empty{C_RESET}");
                 }
-                
+
                 Server::new(
                     port_range,
                     OPTIONS.server_options.secret.as_deref(),
                     OPTIONS.server_options.control_port,
                     OPTIONS.server_options.require_id,
                     Vec::new(),
-                    OPTIONS.server_options.tunnels_addr.clone()
-                ).listen().await?;
+                    OPTIONS.server_options.tunnels_addr.clone(),
+                )
+                .listen()
+                .await?;
             }
-
         }
         Command::Auth => auth(Auth::strawberry_id()).await?,
         Command::About => commands::about::about(),
